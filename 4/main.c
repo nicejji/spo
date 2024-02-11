5473362daa97fa01933fbf233eb0b957ac1af029
Maxim Kozhukh <nicejji@Maxims-MacBook-Air.local>
2024-02-11 16:54:34 +0300

    Remove unused libraries

--- 4/main.c
+++ 4/main.c
@@ -1,11 +1,9 @@
 #include <mpi.h>
-#include <pthread.h>
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
-#include <unistd.h>
 
 #define MIN_NUM -10.0
 #define MAX_NUM 10.0
