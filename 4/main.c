1fdf537933ed0617680ab891e408b8a3c6cbc61c
Maxim Kozhukh <nicejji@Maxims-MacBook-Air.local>
2024-02-11 16:48:52 +0300

    MPI version

--- 4/main.c
+++ 4/main.c
@@ -1,3 +1,4 @@
+#include <mpi.h>
 #include <pthread.h>
 #include <stdbool.h>
 #include <stdio.h>
@@ -16,11 +17,12 @@ void mat_fill_rand(float *m, int size) {
 
 void mat_print(float *m, int size) {
   for (int i = 0; i < size; i++) {
-    printf("| ");
+    printf("│ ");
     for (int j = 0; j < size; j++)
       printf("\x1b[33m%7.2f\x1b[0m ", m[i * size + j]);
-    printf("|\n");
+    printf("│\n");
   }
+  printf("\n");
 }
 
 // All matrices are squared
@@ -81,62 +83,121 @@ void mat_mul_threaded(float *a, float *b, float *c, int size) {
 }
 
 int main(int argc, char *argv[]) {
-  // Default args
-  bool is_threaded = false;
+  MPI_Init(&argc, &argv);
+  int num_cores;
+  int my_id;
+  MPI_Comm_size(MPI_COMM_WORLD, &num_cores);
+  MPI_Comm_rank(MPI_COMM_WORLD, &my_id);
+
+  int size = 10;
   bool is_logged = false;
-  int size = 1000;
 
   // Parse args
   for (int i = 1; i < argc; i++) {
-    if (strcmp(argv[i], "threaded") == 0)
-      is_threaded = true;
-    if (strcmp(argv[i], "logged") == 0)
-      is_logged = true;
     if (strncmp(argv[i], "size=", 5) == 0) {
       char *rest = argv[i] + 5;
       int new_size = atoi(rest);
       if (new_size != 0)
         size = new_size;
     }
+    if (strcmp(argv[i], "logged") == 0)
+      is_logged = true;
   }
 
-  // Allocate memory on the heap
-  float *a = malloc(sizeof(float) * size * size);
-  float *b = malloc(sizeof(float) * size * size);
-  float *c = malloc(sizeof(float) * size * size);
+  int rows_per_worker = size / (num_cores - 1);
+  int rows_remainder = size % (num_cores - 1);
 
-  // Print size
-  printf("Size: %d\n", size);
+  if (my_id == 0) {
+    // I am root proccess
 
-  // Fill matrices
-  srand(time(NULL));
-  rand();
-  mat_fill_rand(a, size);
-  mat_fill_rand(b, size);
+    // Allocate memory for matrices
+    float *a = malloc(sizeof(float) * size * size);
+    float *b = malloc(sizeof(float) * size * size);
+    float *c = malloc(sizeof(float) * size * size);
 
-  // Proccess
-  if (is_threaded)
-    mat_mul_threaded(a, b, c, size);
-  else
-    mat_mul(a, b, c, size);
+    // Fill matrices with random values
+    srand(time(NULL));
+    rand();
+    mat_fill_rand(a, size);
+    mat_fill_rand(b, size);
 
-  // Log results
-  if (is_logged) {
-    printf("\nResults:\n");
-    printf("A = \n\n");
-    mat_print(a, size);
-    printf("\n");
-    printf("B = \n\n");
-    mat_print(b, size);
-    printf("\n");
-    printf("C = A * B = \n\n");
-    mat_print(c, size);
+    // Send columns to workers
+    for (int i = 1; i < num_cores; i++) {
+      MPI_Send(b, size * size, MPI_FLOAT, i, 0, MPI_COMM_WORLD);
+    }
+    // Send rows to workers
+    for (int i = 1; i < num_cores; i++) {
+      bool is_last = i == num_cores - 1;
+      float *rows = a + (i - 1) * size * rows_per_worker;
+      int rows_num = rows_per_worker + (is_last ? rows_remainder : 0);
+      int chunk_size = size * rows_num;
+      MPI_Send(rows, chunk_size, MPI_FLOAT, i, 1, MPI_COMM_WORLD);
+    }
+
+    // Accept calculated rows
+    for (int i = 1; i < num_cores; i++) {
+      bool is_last = i == num_cores - 1;
+      float *calculated = c + (i - 1) * size * rows_per_worker;
+      int rows_num = rows_per_worker + (is_last ? rows_remainder : 0);
+      int chunk_size = size * rows_num;
+      MPI_Recv(calculated, chunk_size, MPI_FLOAT, i, 0, MPI_COMM_WORLD,
+               MPI_STATUS_IGNORE);
+    }
+
+    // Log results
+    if (is_logged) {
+      printf("\nResults:\n");
+      printf("\n");
+      printf("A = \n\n");
+      mat_print(a, size);
+      printf("\n");
+      printf("B = \n\n");
+      mat_print(b, size);
+      printf("\n");
+      printf("C = A * B = \n\n");
+      mat_print(c, size);
+      printf("\n");
+    }
+    MPI_Finalize();
+    // Cleanup memory
+    free(c);
+    free(b);
+    free(a);
+  } else {
+    // Recieve data
+    bool is_last = my_id == num_cores - 1;
+    int rows_num = rows_per_worker + (is_last ? rows_remainder : 0);
+    int chunk_size = size * rows_num;
+
+    float *columns = malloc(size * size * sizeof(float));
+    float *rows = malloc(chunk_size * sizeof(float));
+    float *calculated = malloc(chunk_size * sizeof(float));
+
+    MPI_Recv(columns, size * size, MPI_FLOAT, 0, 0, MPI_COMM_WORLD,
+             MPI_STATUS_IGNORE);
+    MPI_Recv(rows, chunk_size, MPI_FLOAT, 0, 1, MPI_COMM_WORLD,
+             MPI_STATUS_IGNORE);
+
+    // Calculate rows
+    for (int i = 0; i < rows_num; i++) {
+      for (int j = 0; j < size; j++) {
+        // Iterate over rows & cols
+        calculated[i * size + j] = 0.0;
+        for (int k = 0; k < size; k++) {
+          calculated[i * size + j] +=
+              rows[i * size + k] * columns[k * size + j];
+        }
+      }
+    }
+
+    // Send calcualted to root
+    MPI_Send(calculated, chunk_size, MPI_FLOAT, 0, 0, MPI_COMM_WORLD);
+
+    MPI_Finalize();
+    // Cleanup memory
+    free(calculated);
+    free(rows);
+    free(columns);
   }
-
-  // Cleanup memory
-  free(c);
-  free(b);
-  free(a);
-
   return 0;
 }
